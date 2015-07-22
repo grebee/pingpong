dbce2b1846336dade8a6af619933573fdd5c89d0
unknown <tz41zy@USCHDWNCN123LMY.nam.corp.gm.com>
2015-07-22 01:19:47 +0100

    changed text on index

--- app.js
+++ app.js
@@ -33,95 +33,127 @@ angular.module('scheduleApp', ['firebase'])
     // function to set the default data
   $scope.reset = function() {    
 
-    fb.$set({
-      monday: {
-        name: 'Monday',
-        slots: {
-          900: {
-            time: '9:00am',
-            booked: false
-          },
-          0110: {
-            time: '11:00am',
-            booked: false
-          },
-          100: {
-            time: '1:00pm',
-            booked: false
-          },
-          300: {
-            time: '3:00pm',
-            booked: false
-          },
-          500: {
-            time: '5:00pm',
-            booked: false
-          },
-          700: {
-            time: '7:00pm',
-            booked: false
-          }
-    	  }
-      },
-      tuesday: {
-        name: 'Tuesday',
-        slots: {
-          900: {
-            time: '9:00am',
-            booked: false
-          },
-          0110: {
-            time: '11:00am',
-            booked: false
-          },
-          100: {
-            time: '1:00pm',
-            booked: false
-          },
-          300: {
-            time: '3:00pm',
-            booked: false
-          },
-          500: {
-            time: '5:00pm',
-            booked: false
-          },
-          700: {
-            time: '7:00pm',
-            booked: false
-          }
-    	  }
-      },
-      wednesday: {
-        name: 'Wednesday',
-        slots: {
-          900: {
-            time: '9:00am',
-            booked: false
-          },
-          0110: {
-            time: '11:00am',
-            booked: false
-          },
-          100: {
-            time: '1:00pm',
-            booked: false
-          },
-          300: {
-            time: '3:00pm',
-            booked: false
-          },
-          500: {
-            time: '5:00pm',
-            booked: false
-          },
-          700: {
-            time: '7:00pm',
-            booked: false
-          }
-    	  }
-      }
-    });    
+    // fb.$set({
+    //   monday: {
+    //     name: 'Monday',
+    //     slots: {
+    //       900: {
+    //         time: '9:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '11:00am',
+    //         booked: false
+    //       },
+    //       100: {
+    //         time: '1:00pm',
+    //         booked: false
+    //       },
+    //       300: {
+    //         time: '3:00pm',
+    //         booked: false
+    //       },
+    //       500: {
+    //         time: '5:00pm',
+    //         booked: false
+    //       },
+    //       700: {
+    //         time: '7:00pm',
+    //         booked: false
+    //       }
+    // 	  }
+    //   },
+    //   tuesday: {
+    //     name: 'Tuesday',
+    //     slots: {
+    //       900: {
+    //         time: '9:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '11:00am',
+    //         booked: false
+    //       },
+    //       100: {
+    //         time: '1:00pm',
+    //         booked: false
+    //       },
+    //       300: {
+    //         time: '3:00pm',
+    //         booked: false
+    //       },
+    //       500: {
+    //         time: '5:00pm',
+    //         booked: false
+    //       },
+    //       700: {
+    //         time: '7:00pm',
+    //         booked: false
+    //       }
+    // 	  }
+    //   },
+    //   wednesday: {
+    //     name: 'Wednesday',
+    //     slots: {
+    //       900: {
+    //         time: '9:00am',
+    //         booked: false
+    //       },
+    //       915: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       930: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       945: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       1000: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '10:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '11:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '11:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '11:00am',
+    //         booked: false
+    //       },
+    //       0110: {
+    //         time: '11:00am',
+    //         booked: false
+    //       },
+    //       100: {
+    //         time: '1:00pm',
+    //         booked: false
+    //       }
+    // 	  }
+    //   }
+    // });    
 
   };
   
